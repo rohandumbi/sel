471b96fe9fc5ee7326e555e5b092ec9a858f51d6
rohghosh <ScoobyDoo_1986>
2017-05-01 11:19:29 +0530

    1. email form implemented. 2. spelling mistakes rectified.

--- js/main.js
+++ js/main.js
@@ -132,6 +132,33 @@ $(document).ready(function($) {
 
 	//initializing WOW
 	new WOW().init();
+
+	//email form
+	/* attach a submit handler to the form */
+	$("#contact-form").submit(function(event) {
+
+		console.log("form submit called");
+
+		/* stop form from submitting normally */
+		event.preventDefault();
+
+		/* get the action attribute from the <form action=""> element */
+		var $form = $( this ),
+			url = $form.attr( 'action' );
+
+		/* Send the data using post with element id name and name2*/
+		var posting = $.post( url, {
+			contact_name: $('#contact_name').val(),
+			contact_email: $('#contact_email').val(),
+			contact_phone: $('#contact_phone').val(),
+			contact_comments: $('#contact_comments').val()
+		} );
+
+		/* Alerts the results */
+		posting.done(function( data ) {
+			alert('Thank you for writing to us.');
+		});
+	});
 		
 });
 
